6f0b3406712baea068cad2b3a4f861dd41c64145
San Verhavert <SVerhavert@ad.ua.ac.be>
2022-03-22 15:25:56 +0100

    scaffolding: added testing

---
+++ test/test.js
@@ -0,0 +1,9 @@
+var expect = require( "chai" ).expect
+
+describe( "testing idiots card game", function(){
+    describe("spec file", function () {
+        it("must be found", function () {
+          expect(true).to.be.true;
+        });
+      });
+} );
