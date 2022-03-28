18d4f638acbc86c64c5bb17f19b1b4ec8cc30320
SanVerhavert <san.verhavert@uantwerpen.be>
2022-03-28 17:34:00 +0200

    added App.js as code entry point; added some pseudo code; added refference in html file

---
+++ scripts/App.js
@@ -0,0 +1,9 @@
+
+
+
+/*Runner:
+    Before start: Possible to change face up cards with hand cards [strategy: 2's, 10's and high cards face up]
+    select first player at random
+    Play next player; Run through players in clockwize fasion
+    end player if no cards
+    end game if only one player with cards */
