6270ca40dd6a594d295baa39143effc956fdbc13
SanVerhavert <san.verhavert@uantwerpen.be>
2022-03-29 16:09:26 +0200

    updated the pseudo code in App.js

--- scripts/App.js
+++ scripts/App.js
@@ -4,6 +4,12 @@
 /*Runner:
     Before start: Possible to change face up cards with hand cards [strategy: 2's, 10's and high cards face up]
     select first player at random
-    Play next player; Run through players in clockwize fasion
-    end player if no cards
+    Run through players in clockwize fasion
+        if phase 0
+            Player: play 3 if you have one; if not pass
+            if no player has a 3; go to 4 [and so on]
+            if first card is played
+                phase = 1
+        else
+            play
     end game if only one player with cards */
